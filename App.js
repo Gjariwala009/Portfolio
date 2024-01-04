6bc0ef0c0096a95232be9d614b68976f66a42a46
Gjariwala009 <gv554747@dal.ca>
2024-01-04 22:48:29 +0530

    website in progress

---
+++ App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Header from './js/Header';
+
+function App() {
+  return (
+      <div className='App'>
+          <Header />
+        </div>
+   
+  );
+}
+
+export default App;
