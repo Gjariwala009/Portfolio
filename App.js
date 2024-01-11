b8762e3f041e8b1e04fc3250359bfc1df4ccdf22
Gjariwala009 <gv554747@dal.ca>
2024-01-11 19:16:24 +0530

    HomePage completed. About page is work in progress

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 // App.js
 import React from 'react';
-import Header from './js/Header';
+// import Header from './js/Header';
+import About from './js/About';
 
 function App() {
   return (
       <div className='App'>
-          <Header />
+          <About />
         </div>
    
   );
